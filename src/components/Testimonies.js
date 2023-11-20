6dd2bbc19a41eb8ed0149af15d65fd9dde92aecc
Kiasivils <128944980+Kiasivils@users.noreply.github.com>
2023-11-19 21:47:25 -0500

    Add files via upload

--- src/components/Testimonies.js
+++ src/components/Testimonies.js
@@ -7,7 +7,6 @@ import './componentsStyles.css';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Avatar } from "@material-ui/core";
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
 
 
